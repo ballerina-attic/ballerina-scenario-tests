1d1d044f7e27742d1d5e7af5ba6bc2efabbb47b0
Manuri Perera <amaya.uom@gmail.com>
2019-10-03 11:43:45 +0530

    Improve error handling

--- test-grid-scripts/database/destroy_database_infrastructure.sh
+++ test-grid-scripts/database/destroy_database_infrastructure.sh
@@ -8,12 +8,23 @@ grand_parent_path=$(dirname ${parent_path})
 . ${grand_parent_path}/util/infra_utils.sh
 . ${grand_parent_path}/util/database_utils.sh
 
-echo "Resource deletion script is being executed !"
-input_dir=${2}
 
-# Read infrastucture cleanup properties to a associative array
-declare -A infra_cleanup_config
-read_infra_cleanup_props ${input_dir} infra_cleanup_config
+destroy_resources() {
+    echo "Resource deletion script is being executed !"
+    input_dir=${2}
 
-db_identifier=${infra_cleanup_config[DatabaseName]}
-delete_database ${db_identifier}
+    # Read infrastucture cleanup properties to a associative array
+    declare -A infra_cleanup_config
+    read_infra_cleanup_props ${input_dir} infra_cleanup_config
+
+    db_identifier=${infra_cleanup_config[DatabaseName]}
+    delete_database ${db_identifier}
+}
+
+if destroy_resources; then
+  echo "Resource cleanup successful!" >&2
+else
+  ret=$?
+  echo "Resource cleanup failed with exit code $ret" >&2
+  exit $ret
+fi
